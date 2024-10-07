72b1a349071cf9f7d233b2deb537937c54140482
William Guilherme Dahlke <williamgd@weg.net>
2024-10-07 10:46:34 -0300

    Installed dependencies and configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function config() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('gis-automation-api')
+    .setDescription('API to create automations for GIS')
+    .setVersion('1.0')
+    .addTag('metrics')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+config();
