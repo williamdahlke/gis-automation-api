ec7ad7fe8749d3ab461b2f529bf928004c669389
William Guilherme Dahlke <williamgd@weg.net>
2024-10-07 16:28:11 -0300

    Installed framework class-transformer

--- src/shared/models/gaugeMetric.ts
+++ src/shared/models/gaugeMetric.ts
@@ -1,7 +1,14 @@
+import { IsInt, Max, Min } from "class-validator";
 import { Metric } from "./metric";
 
 export class GaugeMetric extends Metric{
-    constructor(public Operation : number){
-        super();        
+    @IsInt()
+    @Min(0)
+    @Max(2)
+    public Operation : number;
+
+    constructor(Operation : number){
+        super();
+        this.Operation = Operation;        
     }
 }
